45c144296acd069e7311fde8678cd4c922ffe386
Feng <2475199146@qq.com>
2016-05-20 17:19:30 +0800

    Add FreeRTOS
    Flash a LED

--- User/main.c
+++ User/main.c
@@ -1,12 +1,47 @@
 #include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx_conf.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "list.h"
+#include "portable.h"
+#include "FreeRTOSConfig.h"
+
+static void vLED1_Task(void *pvParameters);
+static void vLED2_Task(void *pvParameters);
+
 
 void LED_Init(void);
 
 int main()
 {
 	LED_Init();
-	GPIO_ResetBits(GPIOE,GPIO_Pin_3);
-	while(1);
+	xTaskCreate(vLED1_Task,(char *)"LED1",configMINIMAL_STACK_SIZE
+		,NULL,tskIDLE_PRIORITY+1,NULL);
+		xTaskCreate(vLED2_Task,(char *)"LED2",configMINIMAL_STACK_SIZE
+		,NULL,tskIDLE_PRIORITY+2,NULL);
+		
+		vTaskStartScheduler();
+		return 0;
+
+}
+
+void vLED1_Task(void *pvParameters)
+{
+	while(1)
+	{
+		GPIO_ResetBits(GPIOE,GPIO_Pin_3);
+		vTaskDelay(1000/portTICK_RATE_MS);
+	}
+}
+
+void vLED2_Task(void *pvParameters)
+{
+	while(1)
+	{
+		vTaskDelay(500/portTICK_RATE_MS);
+		GPIO_SetBits(GPIOE,GPIO_Pin_3);		
+	}
 }
 
 void LED_Init(void)
